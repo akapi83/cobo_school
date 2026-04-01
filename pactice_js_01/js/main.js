8ebacc87a55d3e19a5187178955f44919efe27ab
akapi83 <akapi83@gmail.com>
2026-04-01 20:51:21 +0900

    ハンバーガーのボタンのJS完成

--- pactice_js_01/js/main.js
+++ pactice_js_01/js/main.js
@@ -1,1 +1,6 @@
 const menuBtn = document.querySelector('.menu_button');
+const spNav = document.querySelector('.sp_nav');
+
+menuBtn.addEventListener('click', () => {
+  spNav.classList.toggle('open');
+});
